2d6a8cc4d7067e498789a1e0dff7708eb752d75b
Hasindu Udara <hasindutechschool@gmail.com>
2025-07-13 21:22:55 +0530

    update

---
+++ 03_DI_With_Spring/src/main/java/lk/ijse/gdse/AppInitializer.java
@@ -0,0 +1,20 @@
+package lk.ijse.gdse;
+
+import lk.ijse.gdse.bean.Boy;
+import lk.ijse.gdse.config.AppConfig;
+import lk.ijse.gdse.di.Test2;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+
+        Test2 test2 = context.getBean(Test2.class);
+        test2.calledHelloMethod();
+
+        context.registerShutdownHook();
+
+    }
+}
